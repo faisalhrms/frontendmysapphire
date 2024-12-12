a9c06dc1f9178ca7afc1e81d72dbe3c5eaa11e17
faisal <faisal.rehman@sapphiretextiles.com.pk>
2024-12-12 18:43:55 +0500

    fix auth endpoint

--- src/modules/auth/redux/authApi.js
+++ src/modules/auth/redux/authApi.js
@@ -8,7 +8,7 @@ export const authApi = createApi({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (credentials) => ({
-                url: '/auth/login',
+                url: 'api/auth/login/',
                 method: 'POST',
                 body: credentials,
             }),
