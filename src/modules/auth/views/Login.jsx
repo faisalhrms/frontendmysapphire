0b3dde85c9a142fde6671d9aacad96e560ddd499
faisal <faisal.rehman@sapphiretextiles.com.pk>
2024-12-12 17:28:52 +0500

    revert dummy

--- src/modules/auth/views/Login.jsx
+++ src/modules/auth/views/Login.jsx
@@ -3,41 +3,22 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import desktopLogoWhite from '@assets/images/brand-logos/desktop-logo.svg';
 import desktopLogoBlack from '@assets/images/brand-logos/desktop-dark.svg';
+import LoginForm from '../components/LoginForm';
 import LoginSlider from "../components/LoginSlider.jsx";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { PMS_ROUTES } from "@modules/project-management/routes.js";
+import React, {useEffect} from "react";
+import {useIsAuthenticated} from "@modules/auth/hooks/authHooks.js";
+import {useNavigate} from "react-router-dom";
+import {PMS_ROUTES} from "@modules/project-management/routes.js";
+
 
 const Login = () => {
   const navigate = useNavigate();
-  const [credentials, setCredentials] = useState({ username: '', password: '' });
-  const [error, setError] = useState('');
-
-  const handleLogin = () => {
-    const dummyUsername = "testUser";
-    const dummyPassword = "password123";
-
-    if (
-      credentials.username === dummyUsername &&
-      credentials.password === dummyPassword
-    ) {
-      localStorage.setItem(
-        "tokens",
-        JSON.stringify({
-          access_token: "dummyAccessToken",
-          access_token_expires: new Date(Date.now() + 60 * 60 * 1000), // 1 hour expiry
-        })
-      );
+  const isAuthenticated = useIsAuthenticated();
+  useEffect(() => {
+    if (isAuthenticated) {
       navigate(PMS_ROUTES.PROJECT.READ.path);
-    } else {
-      setError("Invalid username or password");
     }
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCredentials({ ...credentials, [name]: value });
-  };
+  }, [isAuthenticated]);
 
   return (
     <>
@@ -54,37 +35,12 @@ const Login = () => {
                   <img src={desktopLogoWhite} alt="" className="authentication-brand desktop-logo" />
                   <img src={desktopLogoBlack} alt="" className="authentication-brand desktop-dark" />
                 </div>
-                <div className="text-center my-[3rem] authentication-barrier">
-                  <span> SIGN IN </span>
-                </div>
-                <div className="mb-4">
-                  <input
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    value={credentials.username}
-                    onChange={handleInputChange}
-                    className="border rounded w-full p-2"
-                  />
-                </div>
-                <div className="mb-4">
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={credentials.password}
-                    onChange={handleInputChange}
-                    className="border rounded w-full p-2"
-                  />
-                </div>
-                {error && <p className="text-red-500 text-sm">{error}</p>}
-                <button
-                  onClick={handleLogin}
-                  className="bg-blue-500 text-white p-2 rounded w-full"
-                >
-                  Login
-                </button>
+
+              <div className="text-center my-[3rem] authentication-barrier">
+                <span> SIGN IN </span>
               </div>
+              <LoginForm />
+            </div>
             </div>
           </div>
         </div>
