04a1e1aac7cdb5958271db71787d0013ce12c1db
rehabzafar <rehab.zafar@srl.com.pk>
2024-12-30 14:16:50 +0500

    worked on replanishment

--- src/modules/dashboards/routes.js
+++ src/modules/dashboards/routes.js
@@ -1,32 +1,32 @@
 export const DASHBOARD_ROUTES = {
     PROJECT: {
         path: '/dashboards/project-management-system',
-        permission: 'pms_dashboard',
+        permission: 'dashboard_pms',
 
     },
-    BI:{
+    BI: {
         path: '/dashboards/business-intelligence',
-        permission: 'bi_dashboard',
+        permission: 'dashboard_bi',
     },
-    CIO:{
+    CIO: {
         path: '/dashboards/chief-information-officer',
-        permission: 'cio_dashboard',
+        permission: 'dashboard_cio',
     },
-    CS:{
+    CS: {
         path: '/dashboards/cyber-security',
-        permission: 'cs_dashboard',
+        permission: 'dashboard_cs',
     },
-    SUBSCRIPTION:{
+    SUBSCRIPTION: {
         path: '/dashboards/subscription',
-         permission:'sms_dashboard'
+        // permission:'dashboard_sms'
     },
     SR: {
         path: '/dashboards/sr',
-        permission: 'sr_dashboard'
+        permission: 'dashboard_sr'
     },
     SR_LIST: {
         path: '/dashboards/sr/sr-list/:status',
-        permission: 'sr_dashboard'
+        permission: 'dashboard_sr'
     }
 }
 
@@ -39,21 +39,21 @@ export const MODULE_ROUTES = [
     {
         path: DASHBOARD_ROUTES.BI.path,
         component: () => import(`/src/modules/dashboards/bi/views/BiDashboard.jsx`),
-        permission:DASHBOARD_ROUTES.BI.permission,
+        permission: DASHBOARD_ROUTES.BI.permission,
     },
     {
         path: DASHBOARD_ROUTES.CIO.path,
         component: () => import(`/src/modules/dashboards/cio/views/CioDashboard.jsx`),
-        permission:DASHBOARD_ROUTES.CIO.permission,
+        permission: DASHBOARD_ROUTES.CIO.permission,
     },
     {
         path: DASHBOARD_ROUTES.CS.path,
         component: () => import(`/src/modules/dashboards/cs/views/CsDashboard.jsx`),
-        permission:DASHBOARD_ROUTES.CS.permission,
+        permission: DASHBOARD_ROUTES.CS.permission,
     },
     {
         path: DASHBOARD_ROUTES.SUBSCRIPTION.path,
-        component:()=>import(`/src/modules/dashboards/sms/views/SubscriptionDashboard.jsx`),
+        component: () => import(`/src/modules/dashboards/sms/views/SubscriptionDashboard.jsx`),
     },
     {
         path: DASHBOARD_ROUTES.SR.path,
