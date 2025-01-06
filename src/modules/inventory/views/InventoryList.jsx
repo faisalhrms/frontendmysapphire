483898dc3528331611919399d61e47a90cfc9a76
SRL\kinza.amjad <kinza.amjad@srl.com.pk>
2025-01-06 15:10:17 +0500

    dashborad ,equipment

--- src/modules/inventory/views/InventoryList.jsx
+++ src/modules/inventory/views/InventoryList.jsx
@@ -5,7 +5,7 @@ import  {INVENTORY_ROUTES} from "@modules/inventory/routes.js";
 
 const InventoryList = () => {
     const buttons = (
-        <div className="flex space-x-2">
+        <div className="grid grid-cols-1 sm:grid-cols-1 ">
             <Link to={INVENTORY_ROUTES.ADD.path} className="hs-dropdown-toggle ti-btn ti-btn-primary-full !py-1 !px-2 !text-[0.75rem]">
                 <i className="ri-add-line font-semibold align-middle"></i> Add Equipment
             </Link>
