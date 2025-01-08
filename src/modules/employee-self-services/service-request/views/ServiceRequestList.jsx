075ba72011de84821b2d857b6ddebe3d2d5a83b9
faisal <faisal.rehman@sapphiretextiles.com.pk>
2025-01-08 18:02:18 +0500

    change action positions

--- src/modules/employee-self-services/service-request/views/ServiceRequestList.jsx
+++ src/modules/employee-self-services/service-request/views/ServiceRequestList.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import PageHeader from "@modules/layouts/includes/PageHeader.jsx";
 import DataTable from "@components/DataTable.jsx";
-import { SELF_SERVICES_ROUTES } from "@modules/employee-self-services/routes.js";
-import { submitServiceRequest } from "@modules/employee-self-services/services/service-request/ServiceRequestServices.js";
-import { formatDate } from "@helpers/dateTime.js";
+import {SELF_SERVICES_ROUTES} from "@modules/employee-self-services/routes.js";
+import {submitServiceRequest} from "@modules/employee-self-services/services/service-request/ServiceRequestServices.js";
+import {formatDate} from "@helpers/dateTime.js";
 
 const ServiceRequestList = () => {
     const navigate = useNavigate();
@@ -21,46 +21,17 @@ const ServiceRequestList = () => {
     const handleSubmit = async (id) => {
         try {
             await submitServiceRequest(id);
-            setSubmittedRows((prev) => ({ ...prev, [id]: true })); 
+            setSubmittedRows((prev) => ({...prev, [id]: true}));
         } catch (error) {
             console.error("Error submitting request:", error.message);
         }
     };
 
     const columns = [
-        { Header: 'SR #', accessor: 'sr_number' },
-        { Header: 'SR Type', accessor: 'sr_type.name' },
-        { Header: 'Location', accessor: 'location.name' },
-        {
-            Header: "Request Title",
-            accessor: "request_title",
-            Cell: ({ value }) => 
-              value ? (value.length > 20 ? `${value.slice(0, 20)}...` : value) : "-"
-          },
-        { Header: 'Requester', accessor: 'reporter' },
-     {
-            Header: "Assignee",
-            accessor: "sr_tasks",
-            Cell: ({value}) => {
-                if (Array.isArray(value) && value.length > 0) {
-                    const allAssignees = value.flatMap(task =>
-                        task.assignees.map(a => a.name)
-                    );
-                    const uniqueAssignees = [...new Set(allAssignees)];
-                    return uniqueAssignees.join(", ");
-                }
-                return "UnAssigned";
-            },
-        },
-        { Header: 'Status', accessor: 'status' },
-        {
-            Header: 'Created at',
-            accessor: row => formatDate(row.created_at) 
-        },
         {
             Header: 'Actions',
-            Cell: ({ row }) => {
-                const { id, is_submitted } = row.original;
+            Cell: ({row}) => {
+                const {id, is_submitted} = row.original;
 
                 return (
                     <div className="flex space-x-2">
@@ -85,11 +56,42 @@ const ServiceRequestList = () => {
                 );
             }
         },
+        {Header: 'SR #', accessor: 'sr_number'},
+        {Header: 'SR Type', accessor: 'sr_type.name'},
+        {Header: 'Location', accessor: 'location.name'},
+        {
+            Header: "Request Title",
+            accessor: "request_title",
+            Cell: ({value}) =>
+                value ? (value.length > 20 ? `${value.slice(0, 20)}...` : value) : "-"
+        },
+        {Header: 'Requester', accessor: 'reporter'},
+        {
+            Header: "Assignee",
+            accessor: "sr_tasks",
+            Cell: ({value}) => {
+                if (Array.isArray(value) && value.length > 0) {
+                    const allAssignees = value.flatMap(task =>
+                        task.assignees.map(a => a.name)
+                    );
+                    const uniqueAssignees = [...new Set(allAssignees)];
+                    return uniqueAssignees.join(", ");
+                }
+                return "UnAssigned";
+            },
+        },
+        {Header: 'Status', accessor: 'status'},
+        {
+            Header: 'Created at',
+            accessor: row => formatDate(row.created_at)
+        },
+
     ];
 
     const buttons = (
         <div className="flex space-x-2">
-            <Link to={SELF_SERVICES_ROUTES.SERVICES.CREATE.path} className="hs-dropdown-toggle ti-btn ti-btn-primary-full !py-1 !px-2 !text-[0.75rem]">
+            <Link to={SELF_SERVICES_ROUTES.SERVICES.CREATE.path}
+                  className="hs-dropdown-toggle ti-btn ti-btn-primary-full !py-1 !px-2 !text-[0.75rem]">
                 <i className="ri-add-line font-semibold align-middle"></i> Create Service
             </Link>
         </div>
@@ -97,7 +99,7 @@ const ServiceRequestList = () => {
 
     return (
         <>
-            <PageHeader currentpage="Employee Self Services" mainpage="Employee Services" />
+            <PageHeader currentpage="Employee Self Services" mainpage="Employee Services"/>
             <DataTable
                 columns={columns}
                 title="Self Services"
