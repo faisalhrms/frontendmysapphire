547c3ecf8d0f52ef5f6f7fcbdab9a9c5fe74eaf1
SRL\kinza.amjad <kinza.amjad@srl.com.pk>
2025-01-09 14:41:51 +0500

    working on discount_card

--- src/modules/employee-self-services/discount-card/components/DiscountForm.jsx
+++ src/modules/employee-self-services/discount-card/components/DiscountForm.jsx
@@ -1,100 +1,5 @@
-// import React from "react";
-// import Logo from "../../../../assets/images/company-logos/sapphire.png";
-// import FormButton from "../../../../components/form/FormButton";
-// import HasPermission from "@components/HasPermission.jsx";
-// import LoadingSpinner from "../../../../components/LoadingSpinner.jsx";
 
-// const DiscountForm = ({
-//   selectedOption,
-//   setSelectedOption,
-//   searchQuery,
-//   setSearchQuery,
-//   handleKeyDown,
-//   isLoading,
-//   handleSearch,
-//   filteredData,
-//   data,
-// }) => {
-//   return (
-//     <div className="col-span-12 xl:col-span-6">
-//       <HasPermission permission="discount_card_management">
-//         <div className="box">
-//           <div className="box-body">
-//             <div className="flex flex-wrap justify-between items-center">
-//               <select
-//                 className="w-full md:w-1/2 px-4 py-2 rounded-md form-control"
-//                 value={selectedOption}
-//                 onChange={(e) => setSelectedOption(e.target.value)}
-//               >
-//                 <option value="card_no">Card Number</option>
-//                 <option value="email">Email</option>
-//               </select>
 
-//               <input
-//                 type="text"
-//                 placeholder={`Search by ${
-//                   selectedOption === "email" ? "Email" : "Card Number"
-//                 }`}
-//                 className="w-full md:w-1/3 px-4 py-2 form-control"
-//                 value={searchQuery}
-//                 onChange={(e) => setSearchQuery(e.target.value)}
-//                 onKeyDown={handleKeyDown}
-//               />
-
-//               <FormButton
-//                 isLoading={isLoading}
-//                 text="Search"
-//                 className="w-full md:w-auto px-4 py-2 bg-primary text-white rounded-md"
-//                 onClick={handleSearch}
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </HasPermission>
-
-//       <div className="flex flex-col items-center space-y-6 mt-10 mb-10 rounded-3xl">
-//         <div className="text-black p-6 rounded-3xl shadow-2xl max-w-full md:max-w-3xl w-full border bg-white">
-//           {isLoading ? (
-            
-//             <LoadingSpinner />
-
-//           ) : (
-//             <>
-//               <div className="flex flex-col items-center mb-4">
-//                 <img src={Logo} alt="Sapphire Logo" className="h-7 w-40 mb-4" />
-//                 <h4 className="text-xl md:text-2xl font-semibold mb-2">
-//                   {filteredData?.name || "N/A"}
-//                 </h4>
-//                 <h6 className="text-sm opacity-80">
-//                   Card No. {filteredData?.card_no || "N/A"}
-//                 </h6>
-//                 {!filteredData && (
-//                   <p className="text-red-500 mt-2">Data not available</p>
-//                 )}
-//               </div>
-
-//               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
-//                 {data?.map(({ title, value }, index) => (
-//                   <div
-//                     key={index}
-//                     className="flex flex-col items-center border rounded-lg p-4 bg-white"
-//                   >
-//                     <div className="flex justify-between w-full">
-//                       <h5 className="text-lg font-semibold">{title}:</h5>
-//                       <h5 className="text-lg font-semibold">{value}</h5>
-//                     </div>
-//                   </div>
-//                 ))}
-//               </div>
-//             </>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DiscountForm;
 import React from "react";
 import Logo from "../../../../assets/images/company-logos/sapphire.png";
 import FormButton from "../../../../components/form/FormButton";
@@ -162,7 +67,6 @@ const DiscountForm = ({
                   {filteredData?.name || "N/A"}
                 </h4>
                 <h6 className="text-sm opacity-80">
-                  {/* Display either email or card number dynamically */}
                   {selectedOption === "email"
                     ? `Email: ${filteredData?.email || "N/A"}`
                     : `Card No: ${filteredData?.card_no || "N/A"}`}
@@ -175,7 +79,6 @@ const DiscountForm = ({
                 )}
               </div>
 
-              {/* Display detailed data */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
                 {data?.map(({ title, value }, index) => (
                   <div
--- src/modules/employee-self-services/discount-card/views/DiscountCard.jsx
+++ src/modules/employee-self-services/discount-card/views/DiscountCard.jsx
@@ -1,115 +1,23 @@
-// import React, { useEffect, useState } from "react";
-// import DiscountForm from "../components/DiscountForm";
-// import PageHeader from "@modules/layouts/includes/PageHeader.jsx";
-// import { useSelector } from "react-redux";
-// import fetchDiscountData from "../../services/discount-card/DiscountCard";
-// import AlertModal from "../../../../components/AlertModal.jsx";
-// import Notify from "@helpers/toastNotifications.js";
-
-// const DiscountCard = () => {
-//   const [selectedOption, setSelectedOption] = useState("email");
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [filteredData, setFilteredData] = useState(null);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [errorMessage, setErrorMessage] = useState("Yes");
-
-//   const user = useSelector((state) => state.auth.user);
-//   console.log(user)
-
-//   const handleFetchData = async (email = "", cardNo = "") => {
-//     setIsLoading(true);
-
-//     try {
-//       if (!email) {
-//         setErrorMessage("Email is required for the search.");
-//         setIsLoading(false);
-//         return;
-//       }
-
-//       setErrorMessage("");
-
-//       const data = await fetchDiscountData(email, cardNo || "");
-
-//       if (data?.card_no === null || data?.card_no === "") {
-//         setErrorMessage("This user has no card number.");
-//       } else {
-//         setFilteredData(data);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//       Notify.error('Login successful!');
-//       setErrorMessage("Failed to fetch data. Please try again.");
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (user?.email) {
-//       handleFetchData(user.email);
-//     }
-//   }, [user?.email]);
-
-//   const handleSearch = () => {
-//     if (selectedOption === "email") {
-//       handleFetchData(searchQuery, "");
-//     } else if (selectedOption === "card_no") {
-//       handleFetchData("", searchQuery);
-//     }
-//   };
-
-//   const handleKeyDown = (e) => {
-//     if (e.key === "Enter") {
-//       handleSearch();
-//     }
-//   };
-
-//   return (
-//     <>
-    
-
-//       <PageHeader
-//         currentpage="Detail Discount Card"
-//         activepage="Discount Card"
-//         mainpage="Discount Card"
-//       />
-//       <DiscountForm
-//         selectedOption={selectedOption}
-//         setSelectedOption={setSelectedOption}
-//         searchQuery={searchQuery}
-//         setSearchQuery={setSearchQuery}
-//         handleKeyDown={handleKeyDown}
-//         isLoading={isLoading}
-//         handleSearch={handleSearch}
-//         filteredData={filteredData}
-//         data={filteredData?.data}
-//       />
-//     </>
-//   );
-// };
-
-// export default DiscountCard;
 import React, { useEffect, useState } from "react";
 import DiscountForm from "../components/DiscountForm";
 import PageHeader from "@modules/layouts/includes/PageHeader.jsx";
 import { useSelector } from "react-redux";
 import fetchDiscountData from "../../services/discount-card/DiscountCard";
-import AlertModal from "../../../../components/AlertModal.jsx";
+
 import Notify from "@helpers/toastNotifications.js";
 
 const DiscountCard = () => {
-  const [selectedOption, setSelectedOption] = useState("email"); // Default search by email
+  const [selectedOption, setSelectedOption] = useState("email");
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
   const user = useSelector((state) => state.auth.user);
-  
+
   const handleFetchData = async (email = "", cardNo = "") => {
     setIsLoading(true);
-    setErrorMessage("");  // Reset the error message before every request
-
+    setErrorMessage("");
     try {
       if (!email && !cardNo) {
         setErrorMessage("Both email and card number cannot be empty.");
@@ -142,15 +50,15 @@ const DiscountCard = () => {
 
   const handleSearch = () => {
     if (selectedOption === "email") {
-      handleFetchData(searchQuery, ""); // Search by email
+      handleFetchData(searchQuery, "");
     } else if (selectedOption === "card_no") {
-      handleFetchData("", searchQuery); // Search by card number
+      handleFetchData("", searchQuery);
     }
   };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      handleSearch(); // Trigger search on Enter key
+      handleSearch();
     }
   };
 
@@ -170,8 +78,8 @@ const DiscountCard = () => {
         isLoading={isLoading}
         handleSearch={handleSearch}
         filteredData={filteredData}
-        data={filteredData?.data} 
-        errorMessage={errorMessage} 
+        data={filteredData?.data}
+        errorMessage={errorMessage}
       />
     </>
   );
