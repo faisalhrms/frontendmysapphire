8ccc538d2a006cc300e7899e99269ce1899140a0
faisal <faisal.rehman@sapphiretextiles.com.pk>
2025-01-13 18:56:27 +0500

    add person view sr dashboard and fix discussion email format

--- src/components/form/FormRichTextarea.jsx
+++ src/components/form/FormRichTextarea.jsx
@@ -1,28 +1,41 @@
-import React, { forwardRef } from 'react';
-import { Controller } from 'react-hook-form';
+import React, { forwardRef, useEffect, useRef } from "react";
+import { Controller } from "react-hook-form";
 import SunEditor from "suneditor-react";
-import ErrorMessage from '@components/form/ErrorMessage.jsx';
+import ErrorMessage from "@components/form/ErrorMessage.jsx";
 
-const FormRichTextArea = forwardRef(({ name, control, errors, placeholder, editorOptions = {}, readOnly, ...rest }, ref) => (
-    <>
-        <label htmlFor={name} className="form-label">{placeholder}</label>
-        <Controller
-            name={name}
-            control={control}
-            render={({ field }) => (
-                <SunEditor
-                    {...field}
-                    ref={ref}
-                    readOnly={readOnly}
-                    setOptions={editorOptions}
-                    onChange={(content) => field.onChange(content)}
-                    defaultValue={field.value || ""}
-                    {...rest}
-                />
-            )}
-        />
-        {errors[name] && <ErrorMessage message={errors[name]?.message} />}
-    </>
-));
+const FormRichTextArea = forwardRef(({ name, control, errors, placeholder, editorOptions = {}, readOnly, ...rest }, ref) => {
+    const editorRef = useRef();
+
+    useEffect(() => {
+        if (editorRef.current) {
+            editorRef.current.editor.core.context.element.wysiwyg.innerHTML = "";
+        }
+    }, [rest.defaultValue]);
+
+    return (
+        <>
+            <label htmlFor={name} className="form-label">{placeholder}</label>
+            <Controller
+                name={name}
+                control={control}
+                render={({ field }) => (
+                    <SunEditor
+                        {...field}
+                        ref={(node) => {
+                            ref && (ref.current = node);
+                            editorRef.current = node;
+                        }}
+                        readOnly={readOnly}
+                        setOptions={editorOptions}
+                        onChange={(content) => field.onChange(content)}
+                        setContents={field.value || ""}
+                        {...rest}
+                    />
+                )}
+            />
+            {errors[name] && <ErrorMessage message={errors[name]?.message} />}
+        </>
+    );
+});
 
 export default FormRichTextArea;
