fd778902e8fe068a8eda3f55039e30890ab42301
faisal <faisal.rehman@sapphiretextiles.com.pk>
2025-01-14 15:34:50 +0500

    change name

--- src/modules/dashboards/sr/components/completedOverdue.jsx
+++ src/modules/dashboards/sr/components/completedOverdue.jsx
@@ -12,7 +12,7 @@ const completedOverdue = ({ data }) => {
         <div className="xl:col-span-6 col-span-6">
             <div className="box">
                 <div className="box-header justify-between">
-                    <div className="box-title">Completed but Overdue Analysis</div>
+                    <div className="box-title">Overdue Completed</div>
                 </div>
                 <div className="box-body">
                     <div id="completedOverdueAnalysis">
