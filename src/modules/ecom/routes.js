187550c6ba9dc09e556a3119168d7a2038f1d291
rehabzafar <rehab.zafar@srl.com.pk>
2025-02-18 11:59:04 +0500

    Exective tab issue fixed

--- src/modules/ecom/routes.js
+++ src/modules/ecom/routes.js
@@ -1,6 +1,6 @@
 import OrderShopify from "@modules/ecom/views/OrderShopify.jsx";
 import AnalysisReport from "@modules/ecom/views/AnalysisReport.jsx";
-import ExecutiveTabs from "@modules/ecom/views/executiveTabs.jsx";
+import ExecutiveTabs from "@modules/ecom/views/ExecutiveTabs.jsx";
 export const ECOM_ROUTES = {
     READ: {
         path: "/module/ecom",
