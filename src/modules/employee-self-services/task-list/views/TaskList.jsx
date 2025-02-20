12a80d21390f15237322cb2dc2c08db040c36bcf
rehabzafar <rehab.zafar@srl.com.pk>
2025-02-20 11:19:42 +0500

    working on pms

--- src/modules/employee-self-services/task-list/views/TaskList.jsx
+++ src/modules/employee-self-services/task-list/views/TaskList.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import PageHeader from "@modules/layouts/includes/PageHeader.jsx";
 import DataTable from "@components/DataTable.jsx";
 import { toTitleCase } from "@helpers/formatters.js";
-import { getBadgeClasses } from "@helpers/badges.js";
+import {getBadgeClasses, getStatusClasses} from "@helpers/badges.js";
 // import TaskListFilter from "@modules/tasks/components/TaskListFilter.jsx";
 import useFilters from "@hooks/useFilters.js";
 import {formatDate} from "@helpers/dateTime.js";
+import AvatarList from "@components/AvatarList.jsx";
+import ProgressBar from "@components/ProgressBar.jsx";
+import Tooltip from "@components/Tooltip.jsx";
 
 const TaskList = () => {
     // const {
@@ -41,30 +44,117 @@ const TaskList = () => {
     // }, [resetFilters, getFilters]);
 
     const columns = [
-        { Header: "Project", accessor: "project_name" },
-        { Header: "Milestone", accessor: "milestone_name" },
-        { Header: "Task", accessor: "task_name" },
+        { Header: "Project", accessor: "project.name", disableSortBy: true,
+            Cell: ({row}) => {
+                const project = row.original.project;
+                return (
+                    <Tooltip
+                        id={`project-tooltip-${project.id}`}
+                        tooltipContent={`Click To View Project: ${project.name}`}
+                    >
+                        <Link
+                            to={`/module/projects/detail/${project.id}`}
+                            >
+                            {project.name}
+                        </Link>
+                    </Tooltip>
+                )
+            },
+        },
+        { Header: "Milestone", accessor: "milestone.name", disableSortBy: true, },
+        { Header: "Task", accessor: "name",
+            Cell: ({row}) => {
+                const task = row.original;
+                return (
+                    <Tooltip
+                        id={`project-tooltip-${task.id}`}
+                        tooltipContent={`Click To View Task: ${task.name}`}
+                    >
+                        <Link
+                            to={`/module/tasks/detail/${task.id}`}
+                            className="font-semibold text-[.875rem]">
+                            {task.name}
+                        </Link>
+                    </Tooltip>
+                )
+            },
+        },
+        {
+            Header: 'Teams',
+            accessor: 'teams',
+            disableSortBy: true,
+            Cell: ({ value }) => (
+                <div className="space-x-1 rtl:space-x-reverse">
+                    {Array.isArray(value) && value.length > 0 && (
+                        [...new Set(value)].map((team, index) => (
+                            <span key={index} className="badge bg-primary/10 text-primary">
+                                {toTitleCase(team.name)}
+                            </span>
+                        ))
+                    )}
+                </div>
+            ),
+        },
+        {
+            Header: 'Person',
+            accessor: 'users',
+            disableSortBy: true,
+            Cell: ({row}) => {
+                const users = row.original.users;
+                return (
+                    <AvatarList users={users} />
+                );
+            },
+        },
         {
             Header: "Deadline",
-            accessor: "task_deadline",
+            accessor: "ended_at",
             Cell: ({ value }) => formatDate(value, "MMM dd, yyyy - HH:mm"),
         },
         {
             Header: "Status",
             accessor: "status",
             Cell: ({ row }) => (
-                <span className={getBadgeClasses(row.original.status)}>
+                <span className={getStatusClasses(row.original.status)}>
                 {toTitleCase(row.original.status)}
             </span>
             ),
         },
         {
             Header: "Completion Date",
-            accessor: "completion_date",
-            Cell: ({ value }) => (value ? formatDate(value, "MMM dd, yyyy") : "N/A"),
+            accessor: "completed_at",
+            Cell: ({ value }) => (value ? formatDate(value, "MMM dd, yyyy") : ""),
         },
-        { Header: "Status Timeline", accessor: "status_completion_timeline" },
-        { Header: "Timeline Group", accessor: "timeline_groups" },
+        { Header: "Status Timeline", accessor: "status_completion_timeline", disableSortBy: true, },
+        { Header: "Timeline Group", accessor: "timeline_groups", disableSortBy: true, },
+        {
+            Header: 'Progress',
+            accessor: 'progress',
+            disableSortBy: true,
+            Cell: ({ row }) => {
+                return (
+                    <ProgressBar
+                        value={row.original.progress}
+                        withStatus={false}
+                    />
+                );
+            },
+        },
+        {
+            Header: 'Tags',
+            accessor: 'tags',
+            Cell: ({ value }) => (
+                <div className="space-x-1 rtl:space-x-reverse">
+                    {Array.isArray(value) && value.length > 0 && (
+                        [...new Set(value)].map((tag, index) => (
+                            <span key={index} className="badge bg-primary/10 text-primary">
+                                {toTitleCase(tag.name)}
+                            </span>
+                        ))
+                    )}
+                </div>
+            ),
+        },
     ];
 
 
