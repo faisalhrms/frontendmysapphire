db0970bdd7291a6767e75320e57b57219e4d6e19
Bilal <bilaliqbal2912112@gmail.com>
2025-02-27 09:56:00 +0500

    minor changes

--- src/modules/inventory/services/inventoryService.js
+++ src/modules/inventory/services/inventoryService.js
@@ -9,7 +9,7 @@ export const equipmentStatuses = [
     { value: 'functional', label: 'Functional' },
     { value: 'lost', label: 'Lost' },
     { value: 'sold_to_employee', label: 'Sold To Employee' },
-    { value: 'write_off', label: 'Write Off' },
+    { value: 'write-off', label: 'Write Off' },
     { value: 'temporary_allocation', label: 'Temporary Allocation' },
 ];
 
