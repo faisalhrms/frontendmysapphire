aeec219f943a3c69a4fcaa181892f4c491497889
Bilal <bilaliqbal2912112@gmail.com>
2025-03-05 20:25:31 +0500

    Dashboard Card Redirection

--- src/modules/dashboards/eq/components/EquipmentStatusCard.jsx
+++ src/modules/dashboards/eq/components/EquipmentStatusCard.jsx
@@ -1,30 +1,32 @@
-// src/modules/dashboards/equipment/components/EquipmentStatusCard.jsx
-
 import React from 'react';
-import { getStatusStyles } from "@helpers/statusStyles.js"; // Ensure this function exists and is correctly implemented
+import { useNavigate } from 'react-router-dom';
+import { getStatusStyles } from "@helpers/statusStyles.js";
 import CountUp from "react-countup";
-import SvgIcon from "@components/SvgIcon.jsx";
-import { toTitleCase } from "@helpers/formatters.js"; // Optional: May not be needed if labels are already formatted
-import { equipmentStatuses } from "@modules/inventory/services/inventoryService.js";
 import InventorySvgIcon from "@components/InventorySvgIcon.jsx";
+import { equipmentStatuses } from "@modules/inventory/services/inventoryService.js";
 
 const EquipmentStatusCard = ({ item }) => {
     const { status, count } = item;
-     // Define styles based on status
+    const navigate = useNavigate();
 
-    // Find the status object from equipmentStatuses array
     const statusObj = equipmentStatuses.find(e => e.value === status);
     const statusLabel = statusObj ? statusObj.label : "-";
     const styles = getStatusStyles(statusLabel);
+
+    const handleCardClick = () => {
+        // Navigate to the EquipmentList page with the selected status as a query parameter
+        navigate(`/module/equipment?status=${status}`);
+    };
+
     return (
-        <div className="box transition-transform transform hover:scale-105 ">
+        <div className="box transition-transform transform hover:scale-105 cursor-pointer" onClick={handleCardClick}>
             <div className="box-body">
                 <div className="flex justify-between items-center ">
                     <div>
-                        <p className="  font-semibold ">
+                        <p className="font-semibold">
                             {statusLabel}
                         </p>
-                        <p className="font-bold text-2xl ">
+                        <p className="font-bold text-2xl">
                             <CountUp end={count} />
                         </p>
                     </div>
