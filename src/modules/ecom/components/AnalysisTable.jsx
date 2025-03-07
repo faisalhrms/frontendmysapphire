ce5c4b5e9df1dff6c7c179d739bb936e13498c54
SRL\kinza.amjad <kinza.amjad@srl.com.pk>
2025-03-07 13:01:12 +0500

    ecom Analysis

--- src/modules/ecom/components/AnalysisTable.jsx
+++ src/modules/ecom/components/AnalysisTable.jsx
@@ -12,60 +12,64 @@ const formatPercentage = (value) => {
 
 const AnalysisTable = ({ title, headers = [], data = [], loading }) => {
     return (
-        <div className="bg-white p-4 shadow-md rounded-lg mb-6 dark:text-gray-200 dark:bg-bodybg">
-        <div className="w-full bg-white rounded-lg shadow-lg overflow-hidden mt-4 mb-4 dark:text-gray-200 dark:bg-bodybg">
-            <div className="p-3 text-lg font-semibold text-gray-900  border-b bg-gray-100 dark:border-gray-700 text-left dark:text-gray-200 dark:bg-bodybg">
-                {title}
+
+
+            <div
+                className="p-4 bg-white shadow-lg rounded-lg mb-6 dark:text-gray-200 dark:bg-bodybg">
+                <div className="flex justify-between items-center mb-4 dark:text-gray-200 dark:bg-bodybg">
+                    <h2 className="text-lg font-semibold dark:text-gray-200 dark:bg-bodybg">Commerce Cloud Order</h2>
+                </div>
+
+                <div className="w-full flex justify-center items-center dark:text-gray-200 dark:bg-bodybg">
+                    {loading ? (
+                        <LoadingSpinner/>
+                    ) : (
+                        <table className="w-full table-fixed border-collapse dark:text-gray-200 dark:bg-bodybg">
+                            <thead className="bg-gray-100 dark:text-gray-200 dark:bg-bodybg">
+                            <tr>
+                                {headers.map((header, index) => (
+                                    <th
+                                        key={index}
+                                        className="border p-2 text-center dark:text-gray-200 dark:bg-bodybg"
+                                    >
+                                        {header.label}
+                                    </th>
+                                ))}
+                            </tr>
+                            </thead>
+                            <tbody className="border hover:bg-gray-50 dark:text-gray-200 dark:bg-bodybg">
+                            {data.length > 0 ? (
+                                data.map((row, rowIndex) => (
+                                    <tr key={rowIndex}
+                                        className="border hover:bg-gray-50 dark:text-gray-200 dark:bg-bodybg">
+                                        {headers.map((header, colIndex) => (
+                                            <td
+                                                key={colIndex}
+                                                className={`border p-2 text-start dark:text-gray-200 dark:bg-bodybg ${
+                                                    header.align === "right" ? "text-right" : "text-left"
+                                                }`}
+                                            >
+                                                {header.accessor === "orderConversion"
+                                                    ? formatPercentage(row[header.accessor]).value
+                                                    : formatNumber(row[header.accessor])}
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan={headers.length}
+                                        className="p-3 text-center text-gray-500 dark:text-gray-200 dark:bg-bodybg">
+                                        No Data Available
+                                    </td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </table>
+                    )}
+                </div>
             </div>
 
-            <div className="w-full flex justify-center items-center dark:text-gray-200 dark:bg-bodybg">
-                {loading ? (
-                    <LoadingSpinner />
-                ) : (
-                    <table className="w-full table-fixed border-collapse dark:text-gray-200 dark:bg-bodybg">
-                        <thead className="bg-gray-200 dark:border-gray-700  text-gray-800 dark:text-gray-200 dark:bg-bodybg">
-                        <tr>
-                            {headers.map((header, index) => (
-                                <th
-                                    key={index}
-                                    className="p-2 border border-gray-400 dark:border-gray-700 text-center dark:text-gray-200 dark:bg-bodybg"
-                                >
-                                    {header.label}
-                                </th>
-                            ))}
-                        </tr>
-                        </thead>
-                        <tbody className="text-gray-800 dark:text-gray-200 dark:bg-bodybg">
-                        {data.length > 0 ? (
-                            data.map((row, rowIndex) => (
-                                <tr key={rowIndex} className="border border-gray-300 dark:border-gray-700 transition hover:bg-gray-100 dark:text-gray-200 dark:bg-bodybg">
-                                    {headers.map((header, colIndex) => (
-                                        <td
-                                            key={colIndex}
-                                            className={`border border-gray-400 dark:border-gray-700 px-4 py-2 dark:text-gray-200 dark:bg-bodybg ${
-                                                header.align === "right" ? "text-right" : "text-left"
-                                            }`}
-                                        >
-                                            {header.accessor === "orderConversion"
-                                                ? formatPercentage(row[header.accessor]).value
-                                                : formatNumber(row[header.accessor])}
-                                        </td>
-                                    ))}
-                                </tr>
-                            ))
-                        ) : (
-                            <tr>
-                                <td colSpan={headers.length} className="p-3 text-center text-gray-500 dark:text-gray-200 dark:bg-bodybg">
-                                    No Data Available
-                                </td>
-                            </tr>
-                        )}
-                        </tbody>
-                    </table>
-                )}
-            </div>
-        </div>
-        </div>
     );
 };
 
