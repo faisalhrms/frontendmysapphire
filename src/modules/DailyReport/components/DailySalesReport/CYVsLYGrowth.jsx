59b1989c777f6b8a710514f3038f8a25e9afa89f
SRL\kinza.amjad <kinza.amjad@srl.com.pk>
2025-04-18 11:25:00 +0500

    dailysalereport(cy /ly growth)

--- src/modules/DailyReport/components/DailySalesReport/CYVsLYGrowth.jsx
+++ src/modules/DailyReport/components/DailySalesReport/CYVsLYGrowth.jsx
@@ -226,10 +226,10 @@ const CYVsLYGrowth = ({ data , loading,setDonwloadData }) => {
 
                             {data?.length > 0 && (
                                 <tr className="dark:text-gray-200 dark:bg-bodybg" style={{ backgroundColor: "#f9f9f9" }}>
-                                    <td className="py-2 px-2 border border-gray-400 text-center font-bold sticky left-0 z-10 dark:text-gray-200 dark:bg-bodybg "
+                                    <td className="sticky left-0 z-20 py-2 px-2 border border-gray-400 text-center font-bold bg-gray-200 dark:text-gray-200 dark:bg-bodybg "
                                  >
                                     </td>
-                                    <td className="py-2 px-2 border border-gray-400 text-center font-bold sticky left-20 z-10 dark:text-gray-200 dark:bg-bodybg "
+                                    <td className="sticky left-16 z-20 py-2 px-2 border border-gray-400 text-center font-bold bg-gray-200 dark:text-gray-200 dark:bg-bodybg "
                                         >
                                         Total
                                     </td>
