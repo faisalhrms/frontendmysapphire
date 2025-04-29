76abfbfaf6053066490242cf8c2c3d82094144ed
rehabzafar <rehab.zafar@srl.com.pk>
2025-04-29 17:15:11 +0500

    worked on offline store performance filter

--- src/modules/DailyReport/components/offlineStorePerformanceFilter/OfflineStorePerformFilter.jsx
+++ src/modules/DailyReport/components/offlineStorePerformanceFilter/OfflineStorePerformFilter.jsx
@@ -11,13 +11,11 @@ const OfflineStorePerformFilter=({ control, errors, clearFilter })=>{
                     <div className="box-body p-4">
                         <div className="flex items-center justify-between gap-4">
                             <div className="flex items-center  gap-4 flex-1">
-
                                 <FormInput
                                     type="date"
-                                    name="to_date"
+                                    name="date"
                                     control={control}
                                     errors={errors}
-                                    placeholder="To Date"
                                 />
 
                             </div>
