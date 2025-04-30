6a9c1111d44f4f3aed5dd5b88a5d82a7c2a8627e
SRL\kinza.amjad <kinza.amjad@srl.com.pk>
2025-04-30 12:16:46 +0500

    pms

--- src/modules/project-management/components/project/ProjectTeam.jsx
+++ src/modules/project-management/components/project/ProjectTeam.jsx
@@ -54,7 +54,8 @@ const ProjectTeam = ({ users }) => {
                 <li key={user.id}>
                   <div className="flex items-center">
                     <div className="me-2">
-                      <Avatar avatar={user.avatar} />
+                      <Avatar avatar={user.avatar}
+                              full_name={user.avatar?.full_name || 'N/A'}/>
                     </div>
                     <div className="flex-grow">
                       <span className="font-semibold">
