627b9bee73bb5bf01fe7521f10908b256dad5217
faisal <faisal.rehman@sapphiretextiles.com.pk>
2025-05-14 11:36:53 +0500

    add order return reprot

--- src/modules/dumps/services/dumps_services.js
+++ src/modules/dumps/services/dumps_services.js
@@ -23,6 +23,14 @@ export const downloadOrderSummaryExcel = async ({ startDate, endDate }) => {
   return data;
 };
 
+export const downloadReturnOrderExcel = async ({ startDate, endDate }) => {
+  const { data } = await api.get("/dumps/oms/fetch_return_order_data/", {
+    params: { date_from: startDate, date_to: endDate },
+    responseType: "blob"
+  });
+  return data;
+};
+
 export const downloadWmsExcel = async ({ startDate, endDate }) => {
   const { data } = await api.get("/dumps/oms/fetch_wms_data/", {
     params: { date_from: startDate, date_to: endDate },
