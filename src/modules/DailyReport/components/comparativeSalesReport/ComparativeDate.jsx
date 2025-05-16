2ae1c1491550ce9bc08aa2beee05d9fc37ae8d23
SRL\kinza.amjad <kinza.amjad@srl.com.pk>
2025-05-16 15:49:07 +0500

    sale

--- src/modules/DailyReport/components/comparativeSalesReport/ComparativeDate.jsx
+++ src/modules/DailyReport/components/comparativeSalesReport/ComparativeDate.jsx
@@ -1,21 +1,24 @@
-import React, {useEffect, useState} from "react";
+
+import React, {useState} from "react";
+import FormInput from "@components/form/FormInput.jsx";
 import FilterButton from "@components/form/FilterButton.jsx";
 import FilterClearButton from "@components/form/FilterClearButton.jsx";
-
+import { useForm, useWatch } from "react-hook-form";
 import {
+    downloadDailySaleReport,
     downloadOfflineStorePerformance
 } from "@modules/DailyReport/services/wiseside_services.js";
-import ReactFlatpickr from 'react-flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import getComparativeReportDates from "@modules/DailyReport/views/utils.js";
-import { CalendarIcon } from "@heroicons/react/16/solid/index.js";
 
-const ComparativeDate = ({ control, errors, clearFilter, filters, hideOnlyComparativePeriod = false , onApplyFilters ,getFilters ,setFilterValue  }) => {
+const ComparativeDate = ({ control,errors, clearFilter,filters }) => {
+
+
     const [isDownloading, setIsDownloading] = useState(false);
+
+
 
     const downloadPDF = async () => {
         try {
-            setIsDownloading(true);
+            setIsDownloading(true)
             const pdfData = await downloadOfflineStorePerformance(filters);
             const blob = new Blob([pdfData], { type: 'application/pdf' });
             const link = document.createElement('a');
@@ -24,50 +27,10 @@ const ComparativeDate = ({ control, errors, clearFilter, filters, hideOnlyCompar
             link.click();
         } catch (error) {
             console.error('Error downloading PDF:', error);
-        } finally {
-            setIsDownloading(false);
+        }finally {
+            setIsDownloading(false)
         }
     };
-
-
-    const date = getComparativeReportDates();
-
-
-    const toDateObj = (d) => (d ? (d instanceof Date ? d : new Date(d)) : null);
-
-    const [dateRange, setDateRange] = useState({
-        startDate: toDateObj(date?.startOfMonth),
-        endDate: toDateObj(date?.today),
-    });
-
-    const [dateRange2, setDateRange2] = useState({
-        startDate: toDateObj(date?.startOfPrevYear),
-        endDate: toDateObj(date?.formattedPrevYearYesterday),
-    });
-
-    // When dateRange changes, update corresponding filters using setFilterValue
-    useEffect(() => {
-        if (setFilterValue) {
-            setFilterValue('from_cy', dateRange.startDate ? formatDate(dateRange.startDate) : null);
-            setFilterValue('to_cy', dateRange.endDate ? formatDate(dateRange.endDate) : null);
-        }
-    }, [dateRange, setFilterValue]);
-
-    useEffect(() => {
-        if (setFilterValue) {
-            setFilterValue('from_ly', dateRange2.startDate ? formatDate(dateRange2.startDate) : null);
-            setFilterValue('to_ly', dateRange2.endDate ? formatDate(dateRange2.endDate) : null);
-        }
-    }, [dateRange2, setFilterValue]);
-
-    // Format date helper: YYYY-MM-DD
-    const formatDate = (d) => {
-        const year = d.getFullYear();
-        const month = String(d.getMonth() + 1).padStart(2, '0');
-        const day = String(d.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    };
-
 
     return (
         <div className="grid grid-cols-12 gap-6">
@@ -77,76 +40,56 @@ const ComparativeDate = ({ control, errors, clearFilter, filters, hideOnlyCompar
                         <div className="flex items-center justify-between gap-4">
                             <div className="flex items-center gap-4 flex-1">
                                 <div className="flex-1">
-                                    <div className="w-full">
-                                        <label className="block text-sm font-medium text-black mb-1 dark:text-gray-200 dark:bg-bodybg">
-                                            Current Period
-                                        </label>
-                                        <div className="relative w-full">
-                                            <CalendarIcon
-                                                className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-800 w-5 h-5 pointer-events-none dark:text-gray-200 dark:bg-bodybg"
-                                            />
-                                            <ReactFlatpickr
-                                                className="pl-10 pr-4 py-2 w-full border border-gray-300  rounded-md dark:text-gray-200 dark:bg-bodybg"
-                                                placeholder="Date Range"
-                                                value={[dateRange?.startDate, dateRange?.endDate]}
-                                                onChange={(dates) => {
-                                                    setDateRange({
-                                                        startDate: dates?.[0] || null,
-                                                        endDate: dates?.[1] || null,
-                                                    });
-                                                }}
-                                                options={{
-                                                    mode: 'range',
-                                                    dateFormat: 'j-M-Y', // format like 1-May-2024
-                                                    showMonths: 2,
-                                                }}
-                                            />
-                                        </div>
-                                    </div>
+                                    <FormInput
+                                        type="date"
+                                        name="startOfMonth"
+                                        placeholder="To Current Period"
+                                        control={control}
+                                        errors={errors}
+                                    />
                                 </div>
-                                {!hideOnlyComparativePeriod && (
-                                    <div className="flex-1">
-                                        <div className="w-full">
-                                            <label className="block text-sm font-medium text-black mb-1 dark:text-gray-200 dark:bg-bodybg">
-                                                Comparative Period
-                                            </label>
-                                            <div className="relative w-full">
-                                                <CalendarIcon
-                                                    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-800 w-5 h-5 pointer-events-none dark:text-gray-200 dark:bg-bodybg"
-                                                />
-                                                <ReactFlatpickr
-                                                    className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-gray-200 dark:bg-bodybg"
-                                                    placeholder="Select Date Range"
-                                                    value={[dateRange2?.startDate, dateRange2?.endDate]}
-                                                    onChange={(dates) =>
-                                                        setDateRange2({
-                                                            startDate: dates?.[0] || null,
-                                                            endDate: dates?.[1] || null,
-                                                        })
-                                                    }
-                                                    options={{
-                                                        mode: 'range',
-                                                        dateFormat: 'j-M-Y', // format like 1-May-2024
-                                                        showMonths: 2,
-                                                    }}
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                )}
+                                <div className="flex-1">
+                                    <FormInput
+                                        type="date"
+                                        placeholder="From Current Period"
+                                        name="date"
+                                        control={control}
+                                        errors={errors}
+                                    />
+                                </div>
+                                <div className="flex-1">
+                                    <FormInput
+                                        type="date"
+                                        placeholder="To Comparative Period"
+                                        name="startOfPrevYear"
+                                        control={control}
+                                        errors={errors}
+                                    />
+                                </div>
+
+                                <div className="flex-1">
+                                    <FormInput
+                                        type="date"
+                                        placeholder="From Comparative Period"
+                                        name="prevYearYesterday"
+                                        control={control}
+                                        errors={errors}
+                                    />
+                                </div>
                             </div>
-                            <div className="flex items-center gap-4 mt-6">
-                                <FilterButton  />
-                                <FilterClearButton onClick={clearFilter} />
-                                <button
-                                    type="button"
-                                    className="ti-btn ti-btn-success !mb-0"
-                                    onClick={downloadPDF}
-                                    disabled={isDownloading}
-                                >
-                                    <i className={`bi bi-file-earmark-pdf ${isDownloading ? "spin" : ""}`}></i>
-                                </button>
-                            </div>
+                            <FilterButton/>
+                            <FilterClearButton onClick={clearFilter}/>
+                            <button
+                                type="button"
+                                className="ti-btn ti-btn-success !mb-0"
+                                onClick={() => downloadPDF()}
+                                disabled={isDownloading}
+
+                            >
+                                <i className={`bi bi-file-earmark-pdf ${isDownloading ? "spin" : ""} `}></i>
+                                {isDownloading ? "" : ""}
+                            </button>
+
                         </div>
                     </div>
                 </div>
@@ -156,3 +99,4 @@ const ComparativeDate = ({ control, errors, clearFilter, filters, hideOnlyCompar
 };
 
 export default React.memo(ComparativeDate);
+
--- src/modules/DailyReport/views/ComparativeSaleReportList.jsx
+++ src/modules/DailyReport/views/ComparativeSaleReportList.jsx
@@ -1,231 +1,68 @@
-// import React, {useCallback, useEffect, useMemo, useState} from "react";
-// import { useFetchWithFilters } from "@hooks/useFetchWithFilters.js";
-// import PageHeader from "@modules/layouts/includes/PageHeader.jsx";
-// import IconTabs from "@components/IconTabs.jsx";
-// import useFilters from "@hooks/useFilters.js";
-// import { getPastDate } from "@helpers/dateTime.js";
-// import AClassFiscal from "@modules/DailyReport/components/comparativeSalesReport/AclassFiscal/ClassOfflineOnlineFiscal.jsx";
-// import ComparativeDate from "@modules/DailyReport/components/comparativeSalesReport/ComparativeDate.jsx";
-// import OnlineSaleList from "@modules/DailyReport/components/comparativeSalesReport/OnlineSale/OnlineSaleList.jsx";
-// import AClassIslamicList from "@modules/DailyReport/components/comparativeSalesReport/AClassIslamic/AClassIslamicList.jsx";
-// import LoadingSpinner from "@components/LoadingSpinner.jsx";
-// import getComparativeReportDates from "@modules/DailyReport/views/utils.js";
-// import { useForm } from "react-hook-form";
-//
-//
-// const ComparativeSaleReportList = () => {
-//     const [activeTab, setActiveTab] = useState("ClassonlineFiscal");
-//     const formatDateToYYYYMMDD = (inputDate) => {
-//         const date = new Date(inputDate);
-//         if (isNaN(date)) {
-//             throw new Error('Invalid date');
-//         }
-//         const year = date.getFullYear();
-//         const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
-//         const day = String(date.getDate()).padStart(2, '0');
-//         return `${year}-${month}-${day}`;
-//     };
-//     const {
-//         today,
-//         startOfMonth,
-//         formattedPrevYearYesterday,
-//         startOfPrevYear,
-//     } = getComparativeReportDates();
-//
-//
-//
-//
-//     const {
-//         control,
-//         handleSubmit,
-//         errors,
-//         getValues,
-//         setValue
-//     } = useForm(
-//         useMemo(() => ({
-//             initialFilters: [
-//                 { name: 'from_cy', defaultValue: getPastDate() },
-//                 { name: 'to_cy', defaultValue: startOfMonth },
-//                 { name: 'from_ly', defaultValue: startOfPrevYear },
-//                 { name: 'to_ly', defaultValue: formattedPrevYearYesterday },
-//             ],
-//         }), [startOfMonth, startOfPrevYear, formattedPrevYearYesterday ])
-//     );
-//
-//     const [filters, setFilters] = useState(getValues());
-//
-//     useEffect(()=>{
-//         setValue ('from_ly', '2025-08-09');
-//     },[])
-//
-//
-//     const { data, isLoading } = useFetchWithFilters(
-//         activeTab === "ClassonlineFiscal" ? '/reporting/comparative/fiscal-sales/' :
-//             activeTab === "Local & Global" ? '/reporting/comparative/online-sales/' :
-//                 activeTab === "AClassIslamic" ? '/reporting/comparative/islamic-sales/' :
-//                     activeTab === "Online Targets Achievement" ? '//' :
-//                         '',
-//         filters
-//     );
-//     const hideOnlyComparativePeriod =  activeTab === "AClassIslamic";
-//
-//     const applyFilters = (newFilters) => {
-//         console.log(newFilters)
-//         setFilters(newFilters);
-//     };
-//
-//     const handleChange = (date1,date2)=>{
-//
-//     }
-//
-//
-//
-//     const onSubmit = useCallback((formData) => {
-//         setFilters(formData);
-//     }, []);
-//
-//     const handleTabChange = (tabId) => {
-//         setActiveTab(tabId);
-//     };
-//     const handleClear = useCallback(() => {
-//         setValue('from_cy', getPastDate());
-//         setValue('to_cy', startOfMonth);
-//         setValue('from_ly', startOfPrevYear);
-//         setValue('to_ly', formattedPrevYearYesterday);
-//     }, [startOfMonth, startOfPrevYear, formattedPrevYearYesterday, setValue]);
-//
-//     const showOnlyCurrentPeriod =  activeTab === "AClassIslamic";
-//
-//     return (
-//         <>
-//             <PageHeader
-//                 currentpage="Comparative Sales Report"
-//                 activepage="Report"
-//                 mainpage="Offline Store Performance"
-//             />
-//
-//             <form onSubmit={handleSubmit(onSubmit)}>
-//                 <ComparativeDate
-//                     filters={filters}
-//                     handleChange={handleChange}
-//                     control={control}
-//                     errors={errors}
-//                     onApplyFilters={applyFilters}
-//                     hideOnlyComparativePeriod={hideOnlyComparativePeriod}
-//                     onClear={handleClear}
-//                     getFilters={getValues}
-//                     setFilterValue={setValue}
-//                 />
-//             </form>
-//
-//             <IconTabs
-//                 tabs={[
-//                     {
-//                         id: "ClassonlineFiscal",
-//                         label: "A Class (Offline) & Online - Fiscal",
-//                         icon: <i className='bx bx-briefcase'></i>,
-//                         content: isLoading ? <LoadingSpinner/> :
-//                             <AClassFiscal data={data} isLoading={isLoading} isActive={activeTab === 'ClassonlineFiscal'}
-//                                           filters={filters}/>
-//                     },
-//                     {
-//                         id: "Local & Global",
-//                         label: "Online Sales - Local & Global",
-//                         icon: <i className='bx bx-globe'></i>,
-//                         content: isLoading ? <LoadingSpinner/> :
-//                             <OnlineSaleList data={data} isLoading={isLoading} isActive={activeTab === 'Local & Global'}
-//                                             filters={filters}/>
-//                     },
-//                     {
-//                         id: "AClassIslamic",
-//                         label: "A Class (Offline) & Online - Islamic",
-//                         icon: <i className='bi bi-book-half'></i>,
-//                         content: isLoading ? <LoadingSpinner/> : <AClassIslamicList data={data} isLoading={isLoading}
-//                                                                                     isActive={activeTab === 'AClassIslamic'}
-//                                                                                     filters={filters}/>
-//                     },
-//                     // {
-//                     //     id: "Online Targets Achievement",
-//                     //     label: "Online Targets Achievement",
-//                     //     icon: <i className='bx bx-target-lock'></i>,
-//                     //     content: isLoading ? <LoadingSpinner /> : <SalesPerformanceTable data={data} isLoading={isLoading} isActive={activeTab === 'Online Targets Achievement'} filters={filters} />
-//                     // },
-//                 ]}
-//                 onTabChange={handleTabChange}
-//             />
-//         </>
-//     );
-// };
-//
-// export default ComparativeSaleReportList;
-import React, { useEffect, useState, useMemo, useCallback } from "react";
+
+import React, { useCallback, useMemo, useState } from "react";
 import { useFetchWithFilters } from "@hooks/useFetchWithFilters.js";
 import PageHeader from "@modules/layouts/includes/PageHeader.jsx";
 import IconTabs from "@components/IconTabs.jsx";
+import useFilters from "@hooks/useFilters.js";
 import { getPastDate } from "@helpers/dateTime.js";
+
+import SalesPerformanceTable from "@modules/DailyReport/components/comparativeSalesReport/OnlineTargetsAchievement/SalesPerformanceTable.jsx";
 import AClassFiscal from "@modules/DailyReport/components/comparativeSalesReport/AclassFiscal/ClassOfflineOnlineFiscal.jsx";
 import ComparativeDate from "@modules/DailyReport/components/comparativeSalesReport/ComparativeDate.jsx";
 import OnlineSaleList from "@modules/DailyReport/components/comparativeSalesReport/OnlineSale/OnlineSaleList.jsx";
 import AClassIslamicList from "@modules/DailyReport/components/comparativeSalesReport/AClassIslamic/AClassIslamicList.jsx";
 import LoadingSpinner from "@components/LoadingSpinner.jsx";
+
 import getComparativeReportDates from "@modules/DailyReport/views/utils.js";
-import { useForm } from "react-hook-form";
 
 const ComparativeSaleReportList = () => {
     const [activeTab, setActiveTab] = useState("ClassonlineFiscal");
+
+
     const {
         today,
-
         startOfMonth,
         formattedPrevYearYesterday,
         startOfPrevYear,
     } = getComparativeReportDates();
 
-    const { control, handleSubmit, errors,   reset, getValues, setValue } = useForm(
-        useMemo(() => ({
-            initialFilters: [
-                { name: 'from_cy', defaultValue: getPastDate() },
-                { name: 'to_cy', defaultValue: startOfMonth },
-                { name: 'from_ly', defaultValue: startOfPrevYear },
-                { name: 'to_ly', defaultValue: formattedPrevYearYesterday },
-            ],
-        }), [startOfMonth, startOfPrevYear, formattedPrevYearYesterday])
+    const {
+        control,
+        handleSubmit,
+        errors,
+        getFilters
+    } = useFilters(
+        useMemo(
+            () => ({
+                initialFilters: [
+                    { name: 'date', defaultValue: getPastDate() },
+                    { name: 'startOfMonth', defaultValue: startOfMonth },
+                    { name: 'startOfPrevYear', defaultValue: startOfPrevYear },
+                    { name: 'prevYearYesterday', defaultValue: formattedPrevYearYesterday },
+                ],
+            }),
+            [startOfMonth, startOfPrevYear, formattedPrevYearYesterday]
+        )
     );
 
-    const [filters, setFilters] = useState(getValues());
-
-    useEffect(() => {
-        setValue('from_ly', '2025-08-09');
-    }, []);
+    const [filters, setFilters] = useState(getFilters());
 
     const { data, isLoading } = useFetchWithFilters(
         activeTab === "ClassonlineFiscal" ? '/reporting/comparative/fiscal-sales/' :
             activeTab === "Local & Global" ? '/reporting/comparative/online-sales/' :
                 activeTab === "AClassIslamic" ? '/reporting/comparative/islamic-sales/' :
-                    '',
+                    activeTab === "Online Targets Achievement" ? '//' :
+                        '',
         filters
     );
 
-    const applyFilters = (newFilters) => {
-        console.log(newFilters);
-        setFilters(newFilters);
-    };
+    const onSubmit = useCallback((formData) => {
+        setFilters(formData);
+    }, []);
 
     const handleTabChange = (tabId) => {
         setActiveTab(tabId);
     };
-
-
-    const handleClear = useCallback(() => {
-        // Reset date range values
-        reset({ startDate: "", endDate: "" });
-
-        // Set default values for other fields using setValue
-        setValue('from_cy', getPastDate());
-        setValue('to_cy', startOfMonth);
-        setValue('from_ly', startOfPrevYear);
-        setValue('to_ly', formattedPrevYearYesterday);
-    }, [startOfMonth, startOfPrevYear, formattedPrevYearYesterday, setValue, reset]);
 
     return (
         <>
@@ -235,14 +72,8 @@ const ComparativeSaleReportList = () => {
                 mainpage="Offline Store Performance"
             />
 
-            <form onSubmit={handleSubmit((formData) => setFilters(formData))}>
-                <ComparativeDate
-                    filters={filters}
-                    control={control}
-                    errors={errors}
-                    onApplyFilters={applyFilters}
-                    onClear={handleClear} // Pass handleClear to ComparativeDate
-                />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <ComparativeDate filters={filters} control={control} errors={errors} />
             </form>
 
             <IconTabs
@@ -251,20 +82,26 @@ const ComparativeSaleReportList = () => {
                         id: "ClassonlineFiscal",
                         label: "A Class (Offline) & Online - Fiscal",
                         icon: <i className='bx bx-briefcase'></i>,
-                        content: isLoading ? <LoadingSpinner /> : <AClassFiscal data={data} isLoading={isLoading} filters={filters} />
+                        content: isLoading ? <LoadingSpinner /> : <AClassFiscal data={data} isLoading={isLoading} isActive={activeTab === 'ClassonlineFiscal'} filters={filters} />
                     },
                     {
                         id: "Local & Global",
                         label: "Online Sales - Local & Global",
                         icon: <i className='bx bx-globe'></i>,
-                        content: isLoading ? <LoadingSpinner /> : <OnlineSaleList data={data} isLoading={isLoading} filters={filters} />
+                        content: isLoading ? <LoadingSpinner /> : <OnlineSaleList data={data} isLoading={isLoading} isActive={activeTab === 'Local & Global'} filters={filters} />
                     },
                     {
                         id: "AClassIslamic",
                         label: "A Class (Offline) & Online - Islamic",
                         icon: <i className='bi bi-book-half'></i>,
-                        content: isLoading ? <LoadingSpinner /> : <AClassIslamicList data={data} isLoading={isLoading} filters={filters} />
-                    }
+                        content: isLoading ? <LoadingSpinner /> : <AClassIslamicList data={data} isLoading={isLoading} isActive={activeTab === 'AClassIslamic'} filters={filters} />
+                    },
+                    {
+                        id: "Online Targets Achievement",
+                        label: "Online Targets Achievement",
+                        icon: <i className='bx bx-target-lock'></i>,
+                        content: isLoading ? <LoadingSpinner /> : <SalesPerformanceTable data={data} isLoading={isLoading} isActive={activeTab === 'Online Targets Achievement'} filters={filters} />
+                    },
                 ]}
                 onTabChange={handleTabChange}
             />
