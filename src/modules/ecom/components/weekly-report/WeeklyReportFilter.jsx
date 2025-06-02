882831eb5db01431259b6abd62c1109c6a4c4900
rehabzafar <rehab.zafar@srl.com.pk>
2025-06-02 16:40:22 +0500

    worked on ecom report

--- src/modules/ecom/components/weekly-report/WeeklyReportFilter.jsx
+++ src/modules/ecom/components/weekly-report/WeeklyReportFilter.jsx
@@ -1,7 +1,6 @@
 import React, {useMemo} from "react";
 import FormInput from "@components/form/FormInput.jsx";
 import FilterButton from "@components/form/FilterButton.jsx";
-import FormSelect from "@components/form/FormSelect.jsx";
 
 const WeeklyReportFilter = ({control, errors, filters, activeTab}) => {
 
@@ -15,33 +14,6 @@ const WeeklyReportFilter = ({control, errors, filters, activeTab}) => {
                     <div className="box-body p-4">
                         <div className="flex items-center justify-between gap-4">
                             <div className="flex items-center gap-4 flex-1">
-                                {
-                                    activeTab === 'landing_page_performance' ?
-                                        <div className="flex-1">
-                                            <FormSelect
-                                                name="top"
-                                                control={control}
-                                                errors={errors}
-                                                options={[
-                                                    {value: 5, label: "Top 5"},
-                                                    {value: 10, label: "Top 10"},
-                                                    {value: 20, label: "Top 20"},
-                                                    {value: 30, label: "Top 30"},
-                                                    {value: 40, label: "Top 40"},
-                                                    {value: 50, label: "Top 50"},
-                                                    {value: 70, label: "Top 70"},
-                                                    {value: 100, label: "Top 100"},
-                                                    {value: 150, label: "Top 150"},
-                                                    {value: 200, label: "Top 200"},
-                                                    {value: 250, label: "Top 250"},
-                                                    {value: 300, label: "Top 300"},
-                                                    {value: 400, label: "Top 400"},
-                                                    {value: 500, label: "Top 500"},
-                                                ]}
-                                                label={false}
-                                            />
-                                        </div>
-                                        :
                                         <div className="flex-1">
                                             <FormInput
                                                 type="date"
@@ -53,8 +25,6 @@ const WeeklyReportFilter = ({control, errors, filters, activeTab}) => {
                                                 label={false}
                                             />
                                         </div>
-
-                                }
                             </div>
                             <div className="flex items-center gap-4 flex-2">
                                 <FilterButton/>
--- src/modules/ecom/views/EcomWeeklyReport.jsx
+++ src/modules/ecom/views/EcomWeeklyReport.jsx
@@ -32,7 +32,7 @@ const EcomWeeklyReport = () =>{
 
     const [filters, setFilters] = useState(getFilters());
 
-    const { data, isLoading } = useFetchWithFilters(
+    const { data, isLoading, refetch } = useFetchWithFilters(
         activeTab === "user_journey" ? '/ecom/weekly-report/user-journey/weekly/' :
             activeTab === "source_based_performance" ? '/ecom/weekly-report/source-based-performance/' :
                 activeTab === "landing_page_performance" ? '/ecom/weekly-report/landing-page-performance/' : '', filters
@@ -50,9 +50,10 @@ const EcomWeeklyReport = () =>{
         <>
             <PageHeader currentpage="Ecom Weekly Report" activepage="Reports" mainpage="Ecom Weekly Report"/>
             {
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <WeeklyReportFilter filters={filters} control={control} errors={errors} activeTab={activeTab}/>
-                </form>
+                activeTab !== 'landing_page_performance' &&
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <WeeklyReportFilter filters={filters} control={control} errors={errors} activeTab={activeTab}/>
+                    </form>
             }
             <IconTabs
                 tabs={[
@@ -87,6 +88,10 @@ const EcomWeeklyReport = () =>{
                             <LandingPagePerformanceTab data={data}
                                                        isLoading={isLoading}
                                                        isActive={'landing_page_performance' === activeTab}
+                                                       control={control}
+                                                       errors={errors}
+                                                       handleSubmit={handleSubmit}
+                                                       onSubmit={onSubmit}
                             />
                         ),
                     },
