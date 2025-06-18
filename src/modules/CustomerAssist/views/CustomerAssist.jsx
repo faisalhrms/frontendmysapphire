18c26096c91849a01a1caf84ad563d82e26850a0
Bilal <bilaliqbal2912112@gmail.com>
2025-06-18 15:50:41 +0500

    Adding New Module Named Customer Assist

---
+++ src/modules/CustomerAssist/views/CustomerAssist.jsx
@@ -0,0 +1,54 @@
+
+import React, { useState } from "react";
+import PageHeader from "@modules/layouts/includes/PageHeader.jsx";
+import IconTabs from "@components/IconTabs.jsx";
+import CustomerAssistForm from "@modules/CustomerAssist/components/CustomerAssistForm.jsx";
+import CustomerAssistList from "@modules/CustomerAssist/views/CustomerAssistList.jsx";
+import { useFetchWithFilters } from "@hooks/useFetchWithFilters.js";
+
+const CustomerAssist = () => {
+    const [activeTab, setActiveTab] = useState("list");
+
+
+    const listApiUrl = activeTab === "list" ? "/customer-assist/datatable/" : null;
+
+    const { data, isLoading, refetch } = useFetchWithFilters(listApiUrl);
+
+    const handleTabChange = (tabId) => {
+        setActiveTab(tabId);
+    };
+
+    const listProps = {};
+    if (activeTab === "list") {
+        listProps.data = data;
+        listProps.isLoading = isLoading;
+        listProps.url = listApiUrl;
+        listProps.refetch = refetch; // if your list component can use refetch
+    }
+    listProps.isActive = activeTab === "list";
+
+    const tabs = [
+        {
+            id: "list",
+            label: "List",
+            icon: <i className="bi bi-list-ul"></i>,
+            content: <CustomerAssistList {...listProps} />,
+        },
+        {
+            id: "form",
+            label: "Search/Create",
+            icon: <i className="bi bi-search"></i>,
+            content: <CustomerAssistForm />,
+        },
+
+    ];
+
+    return (
+        <>
+            <PageHeader currentpage="Customer Assist" mainpage="Support" />
+            <IconTabs tabs={tabs} onTabChange={handleTabChange} activeTab={activeTab} />
+        </>
+    );
+};
+
+export default CustomerAssist;
