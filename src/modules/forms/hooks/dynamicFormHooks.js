7ff068c6b4f014a4dfa8ab9ac379d3e77551aeee
rehabzafar <rehab.zafar@srl.com.pk>
2025-06-20 03:41:37 +0500

    working on forms

--- src/modules/forms/hooks/dynamicFormHooks.js
+++ src/modules/forms/hooks/dynamicFormHooks.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import api from "@config/axiosConfig.js";
 import Notify from "@helpers/toastNotifications.js";
+import {useQuery} from "@tanstack/react-query";
 
 export const useDynamicFormEdit = (id) => {
     const [data, setData] = useState(null);
@@ -20,6 +21,24 @@ export const useDynamicFormEdit = (id) => {
 
         fetchData();
     }, [id]);
+
+    return { data, isLoading };
+};
+
+const fetchDynamicFormColumns = async (id) => {
+    const response = await api.get(`/forms/${id}/columns/`);
+    return response.data.data;
+};
+
+export const useDynamicFormColumns = (id) => {
+    const { data, isLoading } = useQuery({
+        queryKey: ['formColumns', id],
+        queryFn: () => fetchDynamicFormColumns(id),
+        enabled: !!id,
+        onError: (error) => {
+            Notify.error(error.response?.data?.message || 'Failed to fetch form columns');
+        },
+    });
 
     return { data, isLoading };
 };
