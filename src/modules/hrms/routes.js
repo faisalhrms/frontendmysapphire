6c7ae7a8fbeadfafbf8df72c34e86e57d053e464
Bilal <bilaliqbal2912112@gmail.com>
2025-08-15 15:49:48 +0500

    Adding New Input Fields

--- src/modules/hrms/routes.js
+++ src/modules/hrms/routes.js
@@ -1,5 +1,6 @@
 import YearSetup from '@modules/hrms/views/YearSetup.jsx';
 import ApprovalSetup from "@modules/hrms/views/ApprovalSetup.jsx";
+import ObjectiveList from "@modules/hrms/views/ObjectiveList.jsx";
 
 export const HRMS_ROUTES = {
   SETUPS: {
@@ -10,8 +11,15 @@ export const HRMS_ROUTES = {
     APPROVAL: {
       path: "/module/hrms/setups/approval-hierarchy",
       permission: "hrms.manage_approval_hierarchy_form"
-    }
+    },
+
   },
+  MANAGEMENT:{
+    OBJECTIVE:{
+      path: "/module/hrms/management/objective",
+      permission: "hrms.manage_approval_hierarchy_form"
+    }
+  }
 };
 
 export const MODULE_ROUTES = [
@@ -24,5 +32,10 @@ export const MODULE_ROUTES = [
     path: HRMS_ROUTES.SETUPS.APPROVAL.path,
     component: ApprovalSetup,
     permission: HRMS_ROUTES.SETUPS.APPROVAL.permission
+  },
+  {
+    path:HRMS_ROUTES.MANAGEMENT.OBJECTIVE.path,
+    component: ObjectiveList,
+    permission: HRMS_ROUTES.SETUPS.OBJECTIVE.permission
   }
 ];
---
+++ src/modules/hrms/views/ObjectiveList.jsx
@@ -0,0 +1,8 @@
+const ObjectiveList = (props) => {
+    return (
+        <>
+        Hello
+        </>
+    )
+}
+export default ObjectiveList;
