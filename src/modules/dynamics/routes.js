601d08ac69a75d55da943366c99a03de63272c29
Bilal <bilaliqbal2912112@gmail.com>
2025-08-31 21:16:03 +0500

    Adding Permissions

--- src/modules/dynamics/routes.js
+++ src/modules/dynamics/routes.js
@@ -8,11 +8,11 @@ export const DYNAMICS_ROUTES = {
    
     READ: {
         path: "/module/dynamics/forms/sweepers-and-guards",
-        // permission: "dynamics.view_sweepersguards",
+         permission: "dynamics.view_sweepers_and_guards",
     },
     ADD: {
         path: "/module/dynamics/forms/sweepers-and-guards/add",
-        // permission: "dynamics.add_sweepersguards",
+         permission: "dynamics.add_sweepers_and_guards",
     },
     DETAIL: {
         path: "/module/dynamics/forms/sweepers-and-guards/detail/:id",
@@ -20,7 +20,7 @@ export const DYNAMICS_ROUTES = {
     },
     EDIT: {
         path: "/module/dynamics/forms/sweepers-and-guards/edit/:id",
-        // permission: "dynamics.change_sweepersguards",
+         permission: "dynamics.change_sweepers_and_guards",
     },
 };
 
@@ -29,21 +29,21 @@ export const MODULE_ROUTES = [
     {
         path: DYNAMICS_ROUTES.READ.path,
         component: SweepersGuardsList,
-        // permission: DYNAMICS_ROUTES.READ.permission,
+         permission: DYNAMICS_ROUTES.READ.permission,
     },
     {
         path: DYNAMICS_ROUTES.ADD.path,
         component: SweepersGardsAdd,
-        // permission: DYNAMICS_ROUTES.ADD.permission,
+         permission: DYNAMICS_ROUTES.ADD.permission,
     },
     {
         path: DYNAMICS_ROUTES.DETAIL.path,
         component: SweepersGuardsList,
-        // permission: DYNAMICS_ROUTES.DETAIL.permission,
+         permission: DYNAMICS_ROUTES.DETAIL.permission,
     },
     {
         path: DYNAMICS_ROUTES.EDIT.path,
         component: SweepersGuardsEdit,
-        // permission: DYNAMICS_ROUTES.EDIT.permission,
+         permission: DYNAMICS_ROUTES.EDIT.permission,
     },
 ];
