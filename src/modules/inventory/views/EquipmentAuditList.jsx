39dbcdd454219c48b1d294cfd06e5b2ae6d56f06
rehabzafar <rehab.zafar@srl.com.pk>
2025-09-03 17:00:12 +0500

    audit

--- src/modules/inventory/views/EquipmentAuditList.jsx
+++ src/modules/inventory/views/EquipmentAuditList.jsx
@@ -1,48 +1,61 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import DataTable from "@components/datatable/DataTable.jsx";
-import { formatDate } from "@helpers/dateTime.js";
-import { Shield } from "lucide-react";
+import {formatDate} from "@helpers/dateTime.js";
+import {Shield} from "lucide-react";
 import IconPageHeader from "@modules/layouts/includes/IconPageHeader.jsx";
 
 const EquipmentAuditList = () => {
     const columns = [
-
-        { Header: "Location", accessor: "location_subnet.location.name", Cell: ({ row }) => row.original.location_subnet?.location?.name || "N/A" },
-        { Header: "Subnet", accessor: "location_subnet.ip", Cell: ({ row }) => row.original.location_subnet?.ip || "N/A" },
-        { Header: "Asset Tag", accessor: "asset_tag" },
-        { Header: "Device Type", accessor: "device_type" },
-        { Header: "OS", accessor: "os" },
-        { Header: "Computer Name", accessor: "computer_name" },
-        { Header: "Domain Name", accessor: "domain_name" },
-        { Header: "Manufacturer", accessor: "manufacturer" },
-        { Header: "System Model", accessor: "system_model" },
-        { Header: "Serial No", accessor: "serial_no" },
-        { Header: "CPU", accessor: "cpu_name" },
-        { Header: "RAM (GB)", accessor: "ram_gb" },
-        { Header: "RAM Slots", accessor: "rams" },
-        { Header: "Disk (GB)", accessor: "hard_disk_gb" },
-        { Header: "Disk Type", accessor: "disk_type" },
-        { Header: "No. of Disks", accessor: "hard_disks" },
-        { Header: "Display Size", accessor: "display_size" },
-        { Header: "MAC Address", accessor: "mac_address" },
-        { Header: "Current User", accessor: "current_user1" },
-        { Header: "Outlook Email", accessor: "outlook_email" },
-        { Header: "Antivirus", accessor: "antivirus" },
-        { Header: "VPN", accessor: "vpn" },
-        { Header: "Email Server", accessor: "email_server" },
-        { Header: "IP Address", accessor: "ip_address" },
+        {
+            Header: "Location",
+            accessor: "location_subnet.location.name",
+            filterType: 'text',
+            filterable: true,
+            filterKey: 'location_subnet__location__name',
+        },
+        {
+            Header: "Subnet",
+            accessor: "location_subnet.ip",
+            filterType: 'text',
+            filterable: true,
+            filterKey: 'location_subnet__ip',
+        },
+        { Header: "Asset Tag", accessor: "asset_tag", filterType: 'text', filterable: true },
+        { Header: "Device Type", accessor: "device_type", filterType: 'text', filterable: true },
+        { Header: "OS", accessor: "os", filterType: 'text', filterable: true },
+        { Header: "Computer Name", accessor: "computer_name", filterType: 'text', filterable: true },
+        { Header: "Domain Name", accessor: "domain_name", filterType: 'text', filterable: true },
+        { Header: "Manufacturer", accessor: "manufacturer", filterType: 'text', filterable: true },
+        { Header: "System Model", accessor: "system_model", filterType: 'text', filterable: true },
+        { Header: "Serial No", accessor: "serial_no", filterType: 'text', filterable: true },
+        { Header: "CPU", accessor: "cpu_name", filterType: 'text', filterable: true },
+        { Header: "RAM (GB)", accessor: "ram_gb", filterType: 'number', filterable: true },
+        { Header: "RAM Slots", accessor: "rams", filterType: 'number', filterable: true },
+        { Header: "Disk (GB)", accessor: "hard_disk_gb", filterType: 'number', filterable: true },
+        { Header: "Disk Type", accessor: "disk_type", filterType: 'text', filterable: true },
+        { Header: "No. of Disks", accessor: "hard_disks", filterType: 'number', filterable: true },
+        { Header: "Display Size", accessor: "display_size", filterType: 'text', filterable: true },
+        { Header: "MAC Address", accessor: "mac_address", filterType: 'text', filterable: true },
+        { Header: "Current User", accessor: "current_user1", filterType: 'text', filterable: true },
+        { Header: "Outlook Email", accessor: "outlook_email", filterType: 'text', filterable: true },
+        { Header: "Antivirus", accessor: "antivirus", filterType: 'text', filterable: true },
+        { Header: "VPN", accessor: "vpn", filterType: 'text', filterable: true },
+        { Header: "Email Server", accessor: "email_server", filterType: 'text', filterable: true },
+        { Header: "IP Address", accessor: "ip_address", filterType: 'text', filterable: true },
         {
             Header: "Created At",
             accessor: "created_at",
+            filterType: 'date',
+            filterable: true,
             Cell: ({ value }) => (value ? formatDate(value) : "N/A"),
         },
         {
             Header: "Updated At",
             accessor: "updated_at",
-            Cell: ({ value }) => (value ? formatDate(value) : "N/A"),
-        },
-        { Header: "Updated By", accessor: "updated_by", Cell: ({ value }) => value ?? "N/A" },
+            filterType: 'date',
+            filterable: true,
+            Cell: ({ value }) => (value ? formatDate(value) : ""),
+        }
     ];
 
 
